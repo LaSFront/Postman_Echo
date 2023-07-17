ed5b4b8d5c66fc2708042f3edc272e98bc1a57e1
“Larisa” <lsergeevna1978@mail.ru>
2023-07-17 15:12:31 +0400

    Initial comit

---
+++ src/test/java/PostmanEchoTest2.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest2 {
+
+    @Test
+    void shouldReturnTextCyrillic() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Привет")
+                .when()
+                .post("/post")
+                .then().log().all()
+                .statusCode(200)
+                .body("data", equalTo("Привет"));
+    }
+}
